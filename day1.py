8ec89f5ac527c68db6410360f110178f24022f03
donatienabafashijwenimana <donaabafashijwe@gmail.com>
2026-03-04 17:03:39 +0200

    commited by adn

--- day1.py
+++ day1.py
@@ -1,54 +1,54 @@
-# num1=int(input('enter num1 ='))
-# num2=int(input("enter num2 ="))
+num1=int(input('enter num1 ='))
+num2=int(input("enter num2 ="))
 
-# #conditional statement
-#      #if :execute statement if is true
-#      #if-else :statement true or false
-#      #if elif : statement that has multiple condition
-#      #nested if : if inside another if
+#conditional statement
+     #if :execute statement if is true
+     #if-else :statement true or false
+     #if elif : statement that has multiple condition
+     #nested if : if inside another if
 
 
-# if num1>num2:
-#   sum =num1+num2
-#   print("num 1 is highet")
-# elif num1 == num2:
-#     print("num 1 equal to num 2")
-# else:
-#     print("num 2 is highet")
+if num1>num2:
+  sum =num1+num2
+  print("num 1 is highet")
+elif num1 == num2:
+    print("num 1 equal to num 2")
+else:
+    print("num 2 is highet")
 
-# #switch case
+#switch case
 
-# day=int(input("enter day "))
-# match(day):
-#     case 1:
-#         print("mon")
-#     case 2:
-#         print("tue")
-#     case 3:
-#         print("wed")
+day=int(input("enter day "))
+match(day):
+    case 1:
+        print("mon")
+    case 2:
+        print("tue")
+    case 3:
+        print("wed")
 
-#     case 4:
-#         print("thu")
-#     case _:
-#         print ("invalid input")
+    case 4:
+        print("thu")
+    case _:
+        print ("invalid input")
 
 
 # loop
-    #for loop
-    #while loop
-    #do while loop
-    #for each
-# i=int(input( "enter i"))
+    for loop
+    while loop
+    do while loop
+    for each
+i=int(input( "enter i"))
 
-# # i=int(input("Enter your num="))
-# for x in ["banana","orange","beans"]:
-#   print(x)
+# i=int(input("Enter your num="))
+for x in ["banana","orange","beans"]:
+  print(x)
 
 # while loop
 
-# i = 10
-# while(i>0):
-#     i-=1
-#     print(i)
+i = 10
+while(i>0):
+    i-=1
+    print(i)
 
 
